3c006edfcf0434b824758a7b8c8d634c5100e875
Rahul Deshmukh <rahul.a.deshmukh@gmail.com>
2018-01-06 08:48:53 -0500

    Added using static to shorten WriteLine calls.

--- ExchangeSharp/Console/ExchangeSharpConsole_Help.cs
+++ ExchangeSharp/Console/ExchangeSharpConsole_Help.cs
@@ -13,6 +13,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 using System;
 using System.Collections.Generic;
+using static System.Console; 
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -24,38 +25,38 @@ namespace ExchangeSharp
     {
         public static void RunShowHelp(Dictionary<string, string> dict)
         {
-            Console.WriteLine("------------------------------------------------------------");
-            Console.WriteLine("ExchangeSharpConsole v. {0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
-            Console.WriteLine("------------------------------------------------------------");
-            Console.WriteLine("Command line arguments should be key=value pairs, separated by space. Please add quotes around any key/value pair with a space in it.");
-            Console.WriteLine();
-            Console.WriteLine("Command categories:");
-            Console.WriteLine("------------------------------------------------------------");
-            Console.WriteLine("help - Show this help screen, or just run without arguments to show help as well.");
-            Console.WriteLine();
-            Console.WriteLine("test - Run integrations test code against exchanges.");
-            Console.WriteLine(" test currently has no additional arguments.");
-            Console.WriteLine();
-            Console.WriteLine("export - export exchange data. CSV files have millisecond timestamp, price and amount columns. The export will also convert the CSV to bin files. This can take a long time depending on your sinceDateTime parameter.");
-            Console.WriteLine(" Please note that not all exchanges will let you do this and may ban your IP if you try to grab to much data at once. I've added sensible sleep statements to limit request rates.");
-            Console.WriteLine(" export exchange=gemini symbol=btcusd path=../../data/gemini sinceDateTime=20150101");
-            Console.WriteLine();
-            Console.WriteLine("convert - convert csv exchange data to bin files for optimized reading. Files are converted in place and csv files are left as is.");
-            Console.WriteLine(" convert symbol=btcusd path=../../data/gemini");
-            Console.WriteLine();
-            Console.WriteLine("stats - show stats from all exchanges. This is a great way to see the price, order book and other useful stats.");
-            Console.WriteLine(" stats currently has no additional arguments.");
-            Console.WriteLine();
-            Console.WriteLine("keys - encrypted API key file utility - this file is only valid for the current user and only on the computer it is created on.");
-            Console.WriteLine(" Create a key file:");
-            Console.WriteLine("  keys mode=create path=pathToKeyFile.bin keylist=key1,key2,key3,key4,etc.");
-            Console.WriteLine("  The keys parameter is comma separated and may contain any number of keys in any order.");
-            Console.WriteLine(" Display a key file:");
-            Console.WriteLine("  keys mode=display path=pathToKeyFile.bin");               
-            Console.WriteLine();
-            Console.WriteLine("example - simple example showing how to create an API instance and get the ticker, and place an order.");
-            Console.WriteLine(" example currently has no additional arguments.");
-            Console.WriteLine();
+            WriteLine("------------------------------------------------------------");
+            WriteLine("ExchangeSharpConsole v. {0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+            WriteLine("------------------------------------------------------------");
+            WriteLine("Command line arguments should be key=value pairs, separated by space. Please add quotes around any key/value pair with a space in it.");
+            WriteLine();
+            WriteLine("Command categories:");
+            WriteLine("------------------------------------------------------------");
+            WriteLine("help - Show this help screen, or just run without arguments to show help as well.");
+            WriteLine();
+            WriteLine("test - Run integrations test code against exchanges.");
+            WriteLine(" test currently has no additional arguments.");
+            WriteLine();
+            WriteLine("export - export exchange data. CSV files have millisecond timestamp, price and amount columns. The export will also convert the CSV to bin files. This can take a long time depending on your sinceDateTime parameter.");
+            WriteLine(" Please note that not all exchanges will let you do this and may ban your IP if you try to grab to much data at once. I've added sensible sleep statements to limit request rates.");
+            WriteLine(" export exchange=gemini symbol=btcusd path=../../data/gemini sinceDateTime=20150101");
+            WriteLine();
+            WriteLine("convert - convert csv exchange data to bin files for optimized reading. Files are converted in place and csv files are left as is.");
+            WriteLine(" convert symbol=btcusd path=../../data/gemini");
+            WriteLine();
+            WriteLine("stats - show stats from all exchanges. This is a great way to see the price, order book and other useful stats.");
+            WriteLine(" stats currently has no additional arguments.");
+            WriteLine();
+            WriteLine("keys - encrypted API key file utility - this file is only valid for the current user and only on the computer it is created on.");
+            WriteLine(" Create a key file:");
+            WriteLine("  keys mode=create path=pathToKeyFile.bin keylist=key1,key2,key3,key4,etc.");
+            WriteLine("  The keys parameter is comma separated and may contain any number of keys in any order.");
+            WriteLine(" Display a key file:");
+            WriteLine("  keys mode=display path=pathToKeyFile.bin");               
+            WriteLine();
+            WriteLine("example - simple example showing how to create an API instance and get the ticker, and place an order.");
+            WriteLine(" example currently has no additional arguments.");
+            WriteLine();
         }
     }
 }
